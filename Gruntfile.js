5ba48c318771161bcfd2a408166807d4c31c4d27
Paul Griffin Petty <me@paulgriffinpetty.com>
2014-05-14 22:40:27 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,132 @@
+module.exports = function (grunt) {
+
+    // Tasks
+    grunt.registerTask('build', ['clean', 'build-js', 'less', 'jinja']);
+    grunt.registerTask('build-js', ['concat:main', 'uglify:all']);
+    grunt.registerTask('default', ['build', 'watch']);
+    grunt.registerTask('lint', ['jslint']);
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // cleans up the compiled JS file
+        clean: {
+            all: [
+                './assets/script/dist',
+                './assets/script/build'
+            ]
+        },
+
+        less: {
+            dev: {
+                options: {
+                    paths: ["/assets/styles"],
+                    rootpath: "",
+                    compress: true,
+                    cleancss: true
+                },
+                files: {
+                    "./assets/style/dist/main.css": "./assets/style/main.less"
+                }
+            }
+        },
+
+        jslint: {
+            dev: {
+                failOnError: false,
+                src: [
+                    'Gruntfile.js',
+                    './assets/script/**/*.js'
+                ],
+                exclude: [
+                    './assets/script/vendor/**/*.js'
+                ],
+                directives: {
+                    browser: true,
+                    predef: [
+                        'jQuery',
+                        '$'
+                    ]
+                }
+            }
+        },
+
+        concat: {
+            main: {
+                src: [
+                    './assets/script/vendor/jquery-1.11.0.js',
+                    //'./assets/script/vendor/bootstrap.js',
+                    //'./assets/script/vendor/owl.carousel.js',
+	                //'./assets/script/vendor/rrssb.js',
+                    './assets/script/main.js'
+                ],
+                dest: './assets/script/build/main.js',
+                nonull: true
+            }
+        },
+
+        uglify: {
+            all: {
+                options: {
+                    preserveComments: 'some',
+                    mangle: {
+                        except: ['jQuery']
+                    }
+                },
+                files: {
+                    './assets/script/dist/main.min.js': [
+                        './assets/script/build/main.js'
+                    ]
+                }
+            }
+        },
+
+        jinja: {
+            dist: {
+                options: {
+                    // templateDirs: ['src/templates']
+                },
+                files: [{
+                    expand: true,
+                    dest: '',
+                    cwd: 'templates/',
+                    src: ['**/!(_)*.html']
+                }]
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+            templates: {
+                files: ['templates/*.html'],
+                tasks: ['jinja'],
+            },
+            css: {
+                files: ['assets/style/*.less'],
+                tasks: ['less'],
+            },
+            css: {
+                files: ['assets/script/*/*.js'],
+                tasks: ['concat', 'uglify'],
+            }
+        },
+
+        shell : {
+        }
+
+    });
+
+    // Includes
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-markdown');
+    grunt.loadNpmTasks('grunt-jinja');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
---
+++ custom-select.js
@@ -0,0 +1,196 @@
+
+// custom-select jQuery plugin
+
+(function($){
+
+    var customSelect = function(element, options){
+
+        var base = this;
+
+        base.$el = $(element);
+        base.el = element;
+
+        base.$select = base.$el.find("select");
+        base.$options = base.$el.find("option");
+
+        base.defaultOptions = {
+
+            debug: false,
+
+            icons: {
+                "open":  "fa-chevron-down",
+                "close": "fa-chevron-up"
+            }
+
+        };
+
+        base.init = function() {
+
+            // skip mobile browsers using this: http://detectmobilebrowser.com/
+            if (typeof jQuery.browser !== "undefined" &&
+                typeof jQuery.browser.mobile !== "undefined" &&
+                jQuery.browser.mobile) {
+                return false;
+            }
+
+            // skip IE8 & older, this looks exactly until <selevct> is clicked
+            // which for IE* & older just displays the browser default <option> menu
+            if (document.all && !document.addEventListener) {
+                return false;
+            }
+
+            base.options = $.extend({}, base.defaultOptions, options);
+
+            base.$el.append("<div class='custom-select-custom-menu'><ul></ul></div>");
+            base.$menu_list = base.$el.find(".custom-select-custom-menu");
+
+            base.$el.on("click", function(event) {
+
+                event.stopPropagation();
+                event.preventDefault();
+
+            });
+
+            base.$select.on("focus mousedown", function(event) {
+
+                event.stopPropagation();
+                event.preventDefault();
+
+                base.toggleCustomSelect();
+
+            });
+
+            base.$options.each(function(i, option) {
+
+                var _text = $(option).text(),
+                    _value = $(option).attr("value");
+
+                // for "no value" options
+                if (typeof _value === "undefined") {
+                    _value = _text;
+                }
+
+                base.$menu_list.find("ul").append("<li data-value='"+_value+"'>"+_text+"</li>");
+
+            });
+
+            base.$el.find("li").on("click", function() {
+
+                var $menu_item = $(this);
+
+                // set original select's value & trigger change event:
+                base.$select.val( $menu_item.data("value") ).trigger("change");
+
+                if (base.options.debug) {
+                    console.log("menu item clicked", $menu_item.data("value") );
+                }
+
+            });
+
+        };
+
+        base.closeCustomSelect = function(target){
+
+            $(target)
+                .removeClass("open")
+                .addClass( base.options.icons.open )
+                .removeClass( base.options.icons.close );
+
+        };
+
+        base.toggleCustomSelect = function(){
+
+            base.$el
+                .toggleClass("open")
+                .toggleClass( base.options.icons.open )
+                .toggleClass( base.options.icons.close );
+
+        };
+
+        base.add = function(_text, _value){
+
+            console.log("customSelect", "add", _text, _value);
+
+            // for "no value" options
+            if (typeof _value === "undefined") {
+                _value = _text;
+            }
+
+            base.$menu_list
+                .find("ul")
+                .append("<li data-value='"+_value+"'>"+_text+"</li>");
+
+            base.$menu_list
+                .find("li[data-value='"+_value+"']")
+                .on("click", function() {
+
+                    var $menu_item = $(this);
+
+                    //console.log( $menu_item, $menu_item.data("value"));
+
+                    // set original select's value & trigger change event:
+                    base.$el.find("select")
+                        .val( $menu_item.data("value") ).trigger("change");
+
+                    if (base.options.debug) {
+                        console.log("menu item clicked", base.$el.find("select").data("value") );
+                    }
+
+                });
+
+            base.$select
+                .append("<option value='"+_value+"'>"+_text+"</option>");
+
+        };
+
+        base.addOptions = function(options_element_map){
+
+            $(options_element_map).each(function(i, option) {
+                base.add(option._text, option._value);
+            });
+
+        };
+
+        base.replaceOptions = function(options_element_map){
+
+            base.$menu_list.find("ul").empty();
+            base.$select.empty();
+
+            base.addOptions(options_element_map);
+
+        };
+
+        base.init();
+
+        // js that needs to run once
+
+            // dismiss select on "blur"
+            $(document).on("click", function() {
+
+                // close them all
+                base.closeCustomSelect( base.el );
+
+            });
+
+
+    };
+
+    $.fn.customSelect = function(options){
+        return this.each(function(){
+
+            var element = $(this);
+
+            // Return early if this element already has a plugin instance
+            if (element.data('customSelect')) return;
+
+            // pass options to plugin constructor
+            var customselect = new customSelect(this, options);
+
+            // Store plugin object in this element's data
+            element.data('customSelect', customselect);
+
+        });
+    };
+
+})(jQuery);
+
